db47d7d3c41fd32a9b3d0afcfe49951101dda439
Saul Gutierrez <saulgtattoos@gmail.com>
2026-05-13 03:20:16 -0700

    feat: add agent columns to crmService toDB and fromDB mapping

--- src/lib/crmService.js
+++ src/lib/crmService.js
@@ -14,20 +14,26 @@ function toDB(client) {
     activityLog,
     createdAt,
     updatedAt,
+    agentIntakeSummary,
+    agentConciergeMessage,
+    agentCallLog,
     ...rest
   } = client
   return {
     ...rest,
-    ...(tattooIdea       !== undefined && { tattoo_idea:         tattooIdea }),
-    ...(nextAction       !== undefined && { next_action:         nextAction }),
-    ...(projectStage     !== undefined && { project_stage:       projectStage }),
-    ...(projectOrder     !== undefined && { project_order:       projectOrder }),
-    ...(consultationCount !== undefined && { consultation_count: consultationCount }),
-    ...(journeyChecklist !== undefined && { journey_checklist:   journeyChecklist }),
-    ...(aftercareChecklist !== undefined && { aftercare_checklist: aftercareChecklist }),
-    ...(activityLog      !== undefined && { activity_log:        activityLog }),
-    ...(createdAt        !== undefined && { created_at:          createdAt }),
-    ...(updatedAt        !== undefined && { updated_at:          updatedAt }),
+    ...(tattooIdea             !== undefined && { tattoo_idea:              tattooIdea }),
+    ...(nextAction             !== undefined && { next_action:              nextAction }),
+    ...(projectStage           !== undefined && { project_stage:            projectStage }),
+    ...(projectOrder           !== undefined && { project_order:            projectOrder }),
+    ...(consultationCount      !== undefined && { consultation_count:       consultationCount }),
+    ...(journeyChecklist       !== undefined && { journey_checklist:        journeyChecklist }),
+    ...(aftercareChecklist     !== undefined && { aftercare_checklist:      aftercareChecklist }),
+    ...(activityLog            !== undefined && { activity_log:             activityLog }),
+    ...(createdAt              !== undefined && { created_at:               createdAt }),
+    ...(updatedAt              !== undefined && { updated_at:               updatedAt }),
+    ...(agentIntakeSummary     !== undefined && { agent_intake_summary:     agentIntakeSummary }),
+    ...(agentConciergeMessage  !== undefined && { agent_concierge_message:  agentConciergeMessage }),
+    ...(agentCallLog           !== undefined && { agent_call_log:           agentCallLog }),
   }
 }
 
@@ -43,20 +49,26 @@ function fromDB(row) {
     activity_log,
     created_at,
     updated_at,
+    agent_intake_summary,
+    agent_concierge_message,
+    agent_call_log,
     ...rest
   } = row
   return {
     ...rest,
-    ...(tattoo_idea        !== undefined && { tattooIdea:         tattoo_idea }),
-    ...(next_action        !== undefined && { nextAction:         next_action }),
-    ...(project_stage      !== undefined && { projectStage:       project_stage }),
-    ...(project_order      !== undefined && { projectOrder:       project_order }),
-    ...(consultation_count !== undefined && { consultationCount:  consultation_count }),
-    ...(journey_checklist  !== undefined && { journeyChecklist:   journey_checklist }),
-    ...(aftercare_checklist !== undefined && { aftercareChecklist: aftercare_checklist }),
-    ...(activity_log       !== undefined && { activityLog:        activity_log }),
-    ...(created_at         !== undefined && { createdAt:          created_at }),
-    ...(updated_at         !== undefined && { updatedAt:          updated_at }),
+    ...(tattoo_idea             !== undefined && { tattooIdea:            tattoo_idea }),
+    ...(next_action             !== undefined && { nextAction:            next_action }),
+    ...(project_stage           !== undefined && { projectStage:          project_stage }),
+    ...(project_order           !== undefined && { projectOrder:          project_order }),
+    ...(consultation_count      !== undefined && { consultationCount:     consultation_count }),
+    ...(journey_checklist       !== undefined && { journeyChecklist:      journey_checklist }),
+    ...(aftercare_checklist     !== undefined && { aftercareChecklist:    aftercare_checklist }),
+    ...(activity_log            !== undefined && { activityLog:           activity_log }),
+    ...(created_at              !== undefined && { createdAt:             created_at }),
+    ...(updated_at              !== undefined && { updatedAt:             updated_at }),
+    ...(agent_intake_summary    !== undefined && { agentIntakeSummary:    agent_intake_summary }),
+    ...(agent_concierge_message !== undefined && { agentConciergeMessage: agent_concierge_message }),
+    ...(agent_call_log          !== undefined && { agentCallLog:          agent_call_log ?? [] }),
   }
 }
 
